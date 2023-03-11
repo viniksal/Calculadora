8d0cd6568dff81cef4120c58278d2602da9396be
Vinícius Khouri Salmeron <viniksalmeron2910@gmail.com>
2023-03-11 18:26:06 -0300

    Alterações no JS

---
+++ calculadora.js
@@ -0,0 +1,55 @@
+
+
+    /*var btn0 = window.document.getElementById('btn0') ISSO FUNCIONA
+
+    btn0.addEventListener('click', clicar)
+
+    function clicar(){
+        btn0.style.background = 'red'; 
+    }*/
+
+
+   /* var btn = document.querySelectorAll("td");  TENTAR ARRUMAR
+
+    btn.addEventListener('click', clicar);
+
+    function clicar(){
+        btn.style.backgroundColor = "red";
+    }*/
+
+    /*//Para cada elemento <td>...
+    btn.forEach((td) => {
+      //...Se texto for "D" colore em vermelho caso contrário colore em verde.
+      td.style.backgroundColor = (td.innerText == 'D') ? "red" : "green";
+    });*/
+
+
+    /*var btn0 = document.querySelectorAll("td")
+
+    btn0.addEventListener('click', clicar)
+
+    function clicar(){
+        btn0.style.background = 'red'
+    }*/
+
+
+        
+        /*a.addEventListener('mousedown', press)
+        .addEventListener('mouseup', unpress)*/
+
+
+
+    /*var btn1 = window.document.getElementById('bnt1')
+    var btn2 = window.document.getElementById('bnt2')
+    var btn3 = window.document.getElementById('bnt3')
+    var btn4 = window.document.getElementById('bnt4')
+    var btn5 = window.document.getElementById('bnt5')
+    var btn6 = window.document.getElementById('bnt6')
+    var btn7 = window.document.getElementById('bnt7')
+    var btn8 = window.document.getElementById('bnt8')
+    var btn9 = window.document.getElementById('bnt9')
+
+    var btnd = window.document.getElementById('bnt/')
+    var btnx = window.document.getElementById('bntx')
+    var btn = window.document.getElementById('bnt')
+    var btn = window.document.getElementById('bnt')*/
